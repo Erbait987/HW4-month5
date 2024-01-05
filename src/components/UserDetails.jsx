0e76e51c689a8916939456b83c282df08e45626a
Erbait987 <erbikaiev@gmail.com>
2024-01-05 17:07:28 +0600

    сделал функционал по передачи данных через библиотеку redux

---
+++ src/components/UserDetails.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const UserDetails = () => {
+  const user = useSelector((state) => state.user);
+
+  return (
+    <div>
+      <h2>User Details</h2>
+      {user.name && user.email && (
+        <>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default UserDetails;
---
+++ src/components/UserForm.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const UserForm = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({ name: "", email: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSave = () => {
+    dispatch({ type: "UPDATE_USER_DATA", payload: formData });
+  };
+
+  return (
+    <div>
+      <h2>User Form</h2>
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleSave}>
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default UserForm;
